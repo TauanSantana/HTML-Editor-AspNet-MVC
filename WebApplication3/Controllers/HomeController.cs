cf4274bf21c51b646780488aa1dda96d52cc6bdf
Tauan Silva <tauan.silva@sinqia.com.br>
2024-10-31 23:29:07 -0300

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string editor1)
+        {
+            // Process the editor content
+            ViewBag.EditorContent = editor1;
+            return View();
+        }
+    }
+}
